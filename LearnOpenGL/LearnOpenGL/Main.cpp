ad1023564a6901244cefdd000f8d3c7d38333abb
Shalia <shalia.wynn730@gmail.com>
2024-08-24 00:38:19 -0500

    Finished chapter on coordinate systems.

--- LearnOpenGL/LearnOpenGL/Main.cpp
+++ LearnOpenGL/LearnOpenGL/Main.cpp
@@ -71,11 +71,56 @@ int main() {
 	};
 
 	float rectangle_00_vertices[] = {
-		// positions          // colors           // texture coords
-		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
-		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
-		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+			// positions			// colors				// texture coords
+			0.5f,  0.5f, 0.0f,		1.0f, 0.0f, 0.0f,		1.0f, 1.0f,	// top right
+			0.5f, -0.5f, 0.0f,		0.0f, 1.0f, 0.0f,		1.0f, 0.0f,	// bottom right
+			-0.5f, -0.5f, 0.0f,		0.0f, 0.0f, 1.0f,		0.0f, 0.0f,	// bottom left
+			-0.5f,  0.5f, 0.0f,		1.0f, 1.0f, 0.0f,		0.0f, 1.0f	// top left 
+	};
+
+	float cube_00_vertices[] = {
+			// positions			// colors				// texture coords
+			-0.5f, -0.5f, -0.5f,	1.0f, 1.0f, 1.0f,		0.0f, 0.0f,
+			0.5f, -0.5f, -0.5f,		1.0f, 1.0f, 1.0f,		1.0f, 0.0f,
+			0.5f,  0.5f, -0.5f,		1.0f, 1.0f, 1.0f,		1.0f, 1.0f,
+			0.5f,  0.5f, -0.5f,		1.0f, 1.0f, 1.0f,		1.0f, 1.0f,
+			-0.5f,  0.5f, -0.5f,	1.0f, 1.0f, 1.0f,		0.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f,	1.0f, 1.0f, 1.0f,		0.0f, 0.0f,
+
+			-0.5f, -0.5f,  0.5f,	1.0f, 1.0f, 1.0f,		0.0f, 0.0f,
+			0.5f, -0.5f,  0.5f,		1.0f, 1.0f, 1.0f,		1.0f, 0.0f,
+			0.5f,  0.5f,  0.5f,		1.0f, 1.0f, 1.0f,		1.0f, 1.0f,
+			0.5f,  0.5f,  0.5f,		1.0f, 1.0f, 1.0f,		1.0f, 1.0f,
+			-0.5f,  0.5f,  0.5f,	1.0f, 1.0f, 1.0f,		0.0f, 1.0f,
+			-0.5f, -0.5f,  0.5f,	1.0f, 1.0f, 1.0f,		0.0f, 0.0f,
+
+			-0.5f,  0.5f,  0.5f,	1.0f, 1.0f, 1.0f,		1.0f, 0.0f,
+			-0.5f,  0.5f, -0.5f,	1.0f, 1.0f, 1.0f,		1.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f,	1.0f, 1.0f, 1.0f,		0.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f,	1.0f, 1.0f, 1.0f,		0.0f, 1.0f,
+			-0.5f, -0.5f,  0.5f,	1.0f, 1.0f, 1.0f,		0.0f, 0.0f,
+			-0.5f,  0.5f,  0.5f,	1.0f, 1.0f, 1.0f,		1.0f, 0.0f,
+
+			0.5f,  0.5f,  0.5f,		1.0f, 1.0f, 1.0f,		1.0f, 0.0f,
+			0.5f,  0.5f, -0.5f,		1.0f, 1.0f, 1.0f,		1.0f, 1.0f,
+			0.5f, -0.5f, -0.5f,		1.0f, 1.0f, 1.0f,		0.0f, 1.0f,
+			0.5f, -0.5f, -0.5f,		1.0f, 1.0f, 1.0f,		0.0f, 1.0f,
+			0.5f, -0.5f,  0.5f,		1.0f, 1.0f, 1.0f,		0.0f, 0.0f,
+			0.5f,  0.5f,  0.5f,		1.0f, 1.0f, 1.0f,		1.0f, 0.0f,
+
+			-0.5f, -0.5f, -0.5f,	1.0f, 1.0f, 1.0f,		0.0f, 1.0f,
+			0.5f, -0.5f, -0.5f,		1.0f, 1.0f, 1.0f,		1.0f, 1.0f,
+			0.5f, -0.5f,  0.5f,		1.0f, 1.0f, 1.0f,		1.0f, 0.0f,
+			0.5f, -0.5f,  0.5f,		1.0f, 1.0f, 1.0f,		1.0f, 0.0f,
+			-0.5f, -0.5f,  0.5f,	1.0f, 1.0f, 1.0f,		0.0f, 0.0f,
+			-0.5f, -0.5f, -0.5f,	1.0f, 1.0f, 1.0f,		0.0f, 1.0f,
+
+			-0.5f,  0.5f, -0.5f,	1.0f, 1.0f, 1.0f,		0.0f, 1.0f,
+			0.5f,  0.5f, -0.5f,		1.0f, 1.0f, 1.0f,		1.0f, 1.0f,
+			0.5f,  0.5f,  0.5f,		1.0f, 1.0f, 1.0f,		1.0f, 0.0f,
+			0.5f,  0.5f,  0.5f,		1.0f, 1.0f, 1.0f,		1.0f, 0.0f,
+			-0.5f,  0.5f,  0.5f,	1.0f, 1.0f, 1.0f,		0.0f, 0.0f,
+			-0.5f,  0.5f, -0.5f,	1.0f, 1.0f, 1.0f,		0.0f, 1.0f
 	};
 
 	unsigned int rectangle_00_indices[] = {
@@ -119,9 +164,9 @@ int main() {
 
 	glBindVertexArray(rectangle_00_VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, rectangle_00_VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(rectangle_00_vertices), rectangle_00_vertices, GL_STATIC_DRAW);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, rectangle_00_EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(rectangle_00_indices), rectangle_00_indices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cube_00_vertices), cube_00_vertices, GL_STATIC_DRAW);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, rectangle_00_EBO);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(rectangle_00_indices), rectangle_00_indices, GL_STATIC_DRAW);
 
 	// Position attribute.
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
@@ -194,6 +239,21 @@ int main() {
 	// uncomment this call to draw in wireframe polygons.
 	// glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
+	glEnable(GL_DEPTH_TEST);
+
+	glm::vec3 cubePositions[] = {
+		glm::vec3(0.0f,  0.0f,  0.0f),
+		glm::vec3(2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3(2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3(1.3f, -2.0f, -2.5f),
+		glm::vec3(1.5f,  2.0f, -2.5f),
+		glm::vec3(1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+
 	// render loop
 	while (!glfwWindowShouldClose(window))
 	{
@@ -202,7 +262,7 @@ int main() {
 
 		// render
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		float timeValue = glfwGetTime();
 		float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
@@ -218,21 +278,54 @@ int main() {
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		shaderProgram_02.use();
-		glm::mat4 rectangle_00_trans = glm::mat4(1.0f);
-		rectangle_00_trans = glm::translate(rectangle_00_trans, glm::vec3(0.5f, -0.5f, 0.0f));
-		rectangle_00_trans = glm::rotate(rectangle_00_trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-		glUniformMatrix4fv(glGetUniformLocation(shaderProgram_02.ID, "transform"), 1, GL_FALSE, glm::value_ptr(rectangle_00_trans));
-		shaderProgram_02.setFloat("mixerValue", mixerValue);
+		//glm::mat4 rectangle_00_trans = glm::mat4(1.0f);
+		//rectangle_00_trans = glm::translate(rectangle_00_trans, glm::vec3(0.5f, -0.5f, 0.0f));
+		//rectangle_00_trans = glm::rotate(rectangle_00_trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+
+		//glm::mat4 model = glm::mat4(1.0f);
+		//model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+
+		glm::mat4 view = glm::mat4(1.0f);
+		// note that we're translating the scene in the reverse direction of where we want to move
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+		glm::mat4 projection;
+		projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+		//glUniformMatrix4fv(glGetUniformLocation(shaderProgram_02.ID, "model"), 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(glGetUniformLocation(shaderProgram_02.ID, "view"), 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(glGetUniformLocation(shaderProgram_02.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+
+		//glUniformMatrix4fv(glGetUniformLocation(shaderProgram_02.ID, "transform"), 1, GL_FALSE, glm::value_ptr(rectangle_00_trans));
+		//shaderProgram_02.setFloat("mixerValue", mixerValue);
 		shaderProgram_02.setFloat("texCoordScale", 2.0f);
 		glBindVertexArray(rectangle_00_VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			if (i == 0 || i % 3 == 0) 
+			{
+				model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			}
+			else 
+			{
+				model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			}
+			glUniformMatrix4fv(glGetUniformLocation(shaderProgram_02.ID, "model"), 1, GL_FALSE, glm::value_ptr(model));
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		shaderProgram_03.use();
 		glm::mat4 rectangle_01_trans = glm::mat4(1.0f);
 		rectangle_01_trans = glm::translate(rectangle_01_trans, glm::vec3(-0.5f, 0.5f, 0.0f));
 		rectangle_01_trans = glm::scale(rectangle_01_trans, abs(sin((float)glfwGetTime() * 0.5f) * glm::vec3(1.0f, 1.0f, 1.0f)));
 		glUniformMatrix4fv(glGetUniformLocation(shaderProgram_03.ID, "transform"), 1, GL_FALSE, glm::value_ptr(rectangle_01_trans));
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 		// GLFW: swap buffers and poll IO events
 		glfwSwapBuffers(window);
--- LearnOpenGL/LearnOpenGL/textured_rectangle_00.vs
+++ LearnOpenGL/LearnOpenGL/textured_rectangle_00.vs
@@ -7,11 +7,14 @@ out vec3 vertexColor;
 out vec2 TexCoord;
 
 uniform mat4 transform;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 uniform float texCoordScale;
 
 void main()
 {
-    gl_Position = transform * vec4(aPos, 1.0);
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
     vertexColor = aColor;
     TexCoord = texCoordScale * aTexCoord;
 }
